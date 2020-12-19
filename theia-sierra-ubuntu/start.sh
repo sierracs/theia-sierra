b4b471f6809fdc674d52e31775d5e157217c03c8
Zed Chance <zchance@csus.edu>
2020-12-19 13:19:50 -0800

    Remove user and group id from command line args
    For demo purposes, each user starts as the theia user, user and group
    id is 1000, and is then renamed. So the new syntax is:
    ./start port_number user_name
    
    For example: ./start 3000 zed
    
    Also, removed all the passwd/group copying in and out of the container,
    it isn't needed right now.

--- theia-sierra-ubuntu/start.sh
+++ theia-sierra-ubuntu/start.sh
@@ -1,9 +1,9 @@
 # Check for command line arguments
-if test $# -lt 4
+if test $# -lt 2
 then
     echo "ERROR: Not enough command line arguments"
-    echo "Usage: $0 port_number user_name user_id group_id"
-    echo "Example: $0 3000 zed 500 20"
+    echo "Usage: $0 port_number user_name"
+    echo "Example: $0 3000 zed"
     exit 1
 fi
 
@@ -13,8 +13,10 @@ IMAGE=zedchance/theia-sierra-ubuntu
 # Command line arguments
 PORT=$1
 WHOAMI=$2
-USER_ID=$3
-GROUP_ID=$4
+
+# Theia user and group ids
+USER_ID=1000
+GROUP_ID=1000
 
 # Create container and get ID
 echo "Creating container from $IMAGE image on port $PORT"
@@ -22,43 +24,11 @@ CONTAINER_ID=$(docker create --security-opt seccomp=unconfined --init -it -p $PO
 SHORT_ID=$(echo $CONTAINER_ID | head -c 5)
 echo "CONTAINER_ID: $SHORT_ID"
 
-# Copy files from container
-echo "Copying passwd and group from container"
-docker cp $CONTAINER_ID:/etc/passwd .
-docker cp $CONTAINER_ID:/etc/group .
-
-# Copy ids into passwd and group
-echo "Adding ids to passwd and group files"
-echo "$WHOAMI:x:$USER_ID:$GROUP_ID:,,,:/home/project:/bin/bash" >> passwd
-echo "$WHOAMI:x:$GROUP_ID:" >> group
-
-# Copy files into container
-echo "Copying back to container"
-docker cp passwd $CONTAINER_ID:/etc/
-docker cp group $CONTAINER_ID:/etc/
-
-# Remove files
-echo "Removing local files"
-rm passwd group
-
 # Start container
 echo "Starting container"
 docker start $CONTAINER_ID
 
-# Setting permissions
-echo "Setting permissions"
-docker exec -u root $CONTAINER_ID chown -R $WHOAMI:$WHOAMI /home/project
+# Rename user to specified whoami
+echo "Renaming user to $WHOAMI"
+docker exec -u root $CONTAINER_ID usermod -l $WHOAMI theia
 
-# Settings
-# if [ ! -d ~/.theia-storage ]
-# then
-#     echo "Making ~/.theia-storage directory with empty settings.json file"
-#     mkdir ~/.theia-storage
-#     echo "{}" > ~/.theia-storage/settings.json
-# fi
-# echo "Linking settings file"
-# docker exec -u root $CONTAINER_ID mkdir -p /home/theia/.theia
-# docker exec -u root $CONTAINER_ID chown -R `whoami`:`whoami` /home/theia/.theia
-# docker exec -u root $CONTAINER_ID ln -fs /home/project/.theia-storage/settings.json /home/theia/.theia/
-
-#CONTAINER_ID="$(docker ps -a --format="{{.Names}})"
