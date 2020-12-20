d12803a9d845449cc4da1ff15ddd1a57c5a4dc5f
Zed Chance <zchance@csus.edu>
2020-12-20 13:41:05 -0800

    Remove port publish, add network flag

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 # Check for command line arguments
-if test $# -lt 2
+if test $# -ne 1
 then
-    echo "ERROR: Not enough command line arguments"
-    echo "Usage: $0 port_number user_name"
-    echo "Example: $0 3000 zed"
+    echo "ERROR: Incorrect command line arguments"
+    echo "Usage: $0 user_name"
+    echo "Example: $0 zed"
     exit 1
 fi
 
@@ -11,16 +11,15 @@ fi
 IMAGE=zedchance/theia-sierra-ubuntu
 
 # Command line arguments
-PORT=$1
-WHOAMI=$2
+WHOAMI=$1
 
 # Theia user and group ids
 USER_ID=1000
 GROUP_ID=1000
 
 # Create container and get ID
-echo "Creating container from $IMAGE image on port $PORT"
-CONTAINER_ID=$(docker create --security-opt seccomp=unconfined --init -it -p 127.0.0.1:$PORT:3000 -u $USER_ID:$GROUP_ID --name $WHOAMI-theia-$PORT $IMAGE)
+echo "Creating container from $IMAGE image"
+CONTAINER_ID=$(docker create --security-opt seccomp=unconfined --init -it --network theia-net -u $USER_ID:$GROUP_ID --name $WHOAMI-theia $IMAGE)
 
 # Start container
 echo "Starting container"
@@ -30,6 +29,7 @@ docker start $CONTAINER_ID
 echo "Renaming user to $WHOAMI"
 docker exec -u root $CONTAINER_ID usermod -l $WHOAMI theia
 
+# Add welcome message to container's project dir
 echo "Copying welcome message"
 docker cp WELCOME.md $CONTAINER_ID:/home/project/
 
