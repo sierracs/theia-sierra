6dc30ffb13cf1bb2cf6d4488cf491026d095ee4a
Zed Chance <zchance@csus.edu>
2020-12-23 14:09:20 -0800

    Add option to publish port, for testing
    The syntax is now:
    
        ./start name [port]
    
    where the port is optional. So,
    
        ./start zed
    
    will create the container and add it to theia-net, and
    
        ./start zed 3000
    
    will create the container and publish it to port 3000.

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 # Check for command line arguments
-if test $# -ne 1
+if test $# -lt 1
 then
     echo "ERROR: Incorrect command line arguments"
-    echo "Usage: $0 user_name"
+    echo "Usage: $0 user_name [port_number]"
     echo "Example: $0 zed"
+    echo "Example: $0 zed 3000"
     exit 1
 fi
 
@@ -12,14 +13,21 @@ IMAGE=zedchance/theia-sierra-ubuntu
 
 # Command line arguments
 WHOAMI=$1
+PORT=$2
 
 # Theia user and group ids
 USER_ID=1000
 GROUP_ID=1000
 
 # Create container and get ID
-echo "Creating container from $IMAGE image"
-CONTAINER_ID=$(docker create --security-opt seccomp=unconfined --init -it --network theia-net -u $USER_ID:$GROUP_ID --name $WHOAMI-theia $IMAGE)
+if test $# -eq 1
+then
+    echo "Creating container from $IMAGE, adding to theia-net"
+    CONTAINER_ID=$(docker create --security-opt seccomp=unconfined --init -it --network theia-net -u $USER_ID:$GROUP_ID --name $WHOAMI-theia $IMAGE)
+else
+    echo "Creating container from $IMAGE on port $PORT"
+    CONTAINER_ID=$(docker create --security-opt seccomp=unconfined --init -it -p 127.0.0.1:$PORT:3000 -u $USER_ID:$GROUP_ID --name $WHOAMI-theia $IMAGE)
+fi
 
 # Start container
 echo "Starting container"
