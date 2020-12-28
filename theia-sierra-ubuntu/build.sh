fd5c075ac261c126680cdd95f3dc71278640acdc
Zed Chance <zchance@csus.edu>
2020-12-28 15:25:01 -0800

    Use buildx, add arch option
    The script will automatically build for the builder's architecture,
    but you can specify which architecture as the first command line arg,
    for example:
    
        ./build amd64

--- theia-sierra-ubuntu/build.sh
+++ theia-sierra-ubuntu/build.sh
@@ -1,2 +1,13 @@
-# Build theia-sierra-ubuntu image command
-docker build --no-cache --build-arg version=sierra -t zedchance/theia-sierra-ubuntu .
+#!/bin/bash
+
+# Build theia-sierra-ubuntu image, defaulting to amd64 arch
+if test $# -lt 1
+then
+    ARCH=$(arch)
+else
+    ARCH=$1
+fi
+
+echo "Building with $ARCH architecture"
+docker buildx build --platform linux/$ARCH -t zedchance/theia-sierra-ubuntu:$ARCH .
+
