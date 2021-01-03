a5811cca6cb109558737002259b61f7277688a16
Zed Chance <zchance@csus.edu>
2021-01-03 08:19:39 -0800

    Fix lint

--- stop_idles.sh
+++ stop_idles.sh
@@ -5,7 +5,7 @@ INACTIVE="All frontend contributions have been stopped"
 TIMEOUT=1800
 
 # Loop thru running containers
-for CONTAINER in $(docker ps --format {{.Names}})
+for CONTAINER in $(docker ps --format "{{.Names}}")
 do
     # Get latest log entry
     LOG=$(docker container logs -t -n 1 "$CONTAINER")
@@ -14,14 +14,17 @@ do
     TIMESTAMP=${LOG:0:19}
 
     # Debug
+    # echo ""
     # echo "Container ID: $CONTAINER"
-    # echo "Log: $LOG"
+    # echo "Log: ${LOG:31:100}"
     # echo "Timestamp: $TIMESTAMP"
 
     # Check to see if the log entry is the inactive message
     if [[ $LOG == *"$INACTIVE"* ]]
     then
         echo "Closed container found: $CONTAINER"
+
+        # Current time
         NOW=$(date +%s)
 
         # Date command on macOS
@@ -31,19 +34,19 @@ do
 
         # Calculate idle time
         # +28800 to compensate for PST to UTC, fix this
-        DIFF=$(expr "$NOW" - "$THEN" + 28800)
-        IDLE_MIN=$(expr $DIFF / 60)
-        echo "    $CONTAINER has been idle for $IDLE_MIN minutes"
+        DIFF=$((NOW - THEN + 28800))
+        IDLE_MIN=$((DIFF / 60))
 
         # Debug
-        # echo "Now : $NOW"
-        # echo "Then: $THEN"
-        # echo "Diff: $DIFF"
+        # echo "    Now : $NOW"
+        # echo "    Then: $THEN"
+        # echo "    Diff: $DIFF"
+        echo "    $CONTAINER has been idle for $IDLE_MIN minutes"
 
         if [[ $DIFF -gt $TIMEOUT ]]
         then
             echo "    $CONTAINER has exceeded time out, stopping..."
-            docker stop $CONTAINER > /dev/null
+            docker stop "$CONTAINER" > /dev/null
         fi
     fi
 done
