f17d181dd99f707369b1a21058321dee230aef41
Zed Chance <zchance@csus.edu>
2021-01-19 15:05:59 -0800

    Tidy theia-base image

--- theia-base/README.md
+++ theia-base/README.md
@@ -1,3 +1,4 @@
 # theia-base
 
-A Dockerfile with the base Theia installation
+A Dockerfile with the base Theia installation, adapted from [theia-cpp-docker](https://github.com/theia-ide/theia-apps/tree/master/theia-cpp-docker).
+
--- theia-base/build.sh
+++ theia-base/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo "Building with amd64 architecture"
-docker buildx build --no-cache --platform linux/amd64 -t zedchance/theia-base:latest .
+docker buildx build --no-cache --platform linux/amd64 -t zedchance/theia-base:dev .
 
